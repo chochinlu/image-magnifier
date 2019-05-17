574804c6f860ba0aef4f72ad77e0f50dd17b265b
Cho-Ching Lu <chochinlu@gmail.com>
2019-05-17 16:02:42 +0800

    Updates

---
+++ precache-manifest.f86ec6ac088027a847e5a54b8f89677e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa34fb076348cc2726aae0a50647fc7",
+    "url": "/image-magnifier/index.html"
+  },
+  {
+    "revision": "aa68976c2fc5d5cae25d",
+    "url": "/image-magnifier/static/js/2.e9bc8905.chunk.js"
+  },
+  {
+    "revision": "4f13b97b0180a7c326c6",
+    "url": "/image-magnifier/static/js/main.f14ba386.chunk.js"
+  },
+  {
+    "revision": "fb9dce758e9c029596e6",
+    "url": "/image-magnifier/static/js/runtime~main.e4474be1.js"
+  }
+]);
