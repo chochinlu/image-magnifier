e1a066587a3f20ca18bd21f8baef9968dc0ed6fb
Cho-Ching Lu <chochinlu@gmail.com>
2019-05-17 15:40:56 +0800

    Add tunning form handling

--- src/components/Magnifier.js
+++ src/components/Magnifier.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Heading, Box, Flex, Text } from 'rebass';
-import { Label, Input } from './style';
+import { Label, Input, StyledLink } from './style';
+import ReactImageMagnify from 'react-image-magnify';
 
-const Slider = ({ title, min, max, step, value, rate }) => (
+const Slider = ({ title, min, max, step, value, onChange }) => (
   <Flex flexDirection="column" justifyContent="center" flex={1} mx={2}>
     <Box>
       <Text fontSize={2}>{title}</Text>
     </Box>
     <Flex mt={1}>
       <Box flex={1}>
-        <Input type="range" min={min} max={max} step={step} value={value} />
+        <Input
+          type="range"
+          min={min}
+          max={max}
+          step={step}
+          value={value}
+          onChange={onChange}
+        />
       </Box>
       <Box>
         <Label>{value}x</Label>
@@ -18,7 +26,89 @@ const Slider = ({ title, min, max, step, value, rate }) => (
   </Flex>
 );
 
+const imageSource = {
+  nasa:
+    'https://images-assets.nasa.gov/image/iss036e009053/iss036e009053~orig.jpg'
+};
+
+const ImageView = ({ smallImageRate, oriImageDimention, enlargeRate }) => (
+  <ReactImageMagnify
+    {...{
+      smallImage: {
+        alt: 'Wristwatch by Ted Baker London',
+        isFluidWidth: false,
+        src: imageSource.nasa,
+        width: oriImageDimention.width * smallImageRate,
+        height: oriImageDimention.height * smallImageRate
+      },
+      largeImage: {
+        src: imageSource.nasa,
+        width: oriImageDimention.width,
+        height: oriImageDimention.height
+      },
+      enlargedImageContainerDimensions: {
+        width: enlargeRate.x,
+        height: enlargeRate.y
+      }
+    }}
+  />
+);
+
+const numToPercent = rate => ({
+  x: `${rate.x * 100}%`,
+  y: `${rate.y * 100}%`
+});
+
 const Magnifier = props => {
+  const [oriImageDimention, setOriImageDimention] = useState({
+    width: 0,
+    height: 0
+  });
+
+  const [smallImageRate, setSmallImageRate] = useState(0.1);
+  const [enlargeRate, setEnlargeRate] = useState({ x: 2, y: 1.5 });
+
+  useEffect(() => {
+    let image = new Image(); // or document.createElement('img')
+    image.onload = function() {
+      const width = this.width;
+      const height = this.height;
+      // console.log({ width, height });
+      setOriImageDimention({ width, height });
+    };
+    image.src = imageSource[props.match.params.img];
+
+    return () => {
+      //unmount image
+      if (!image) {
+        return;
+      }
+      image.onload = function() {};
+      image = null;
+    };
+  });
+
+  const handleSmallImageRate = e => {
+    e.preventDefault();
+    setSmallImageRate(e.target.value);
+  };
+
+  const handleChangeEnlargeRateX = e => {
+    e.preventDefault();
+    setEnlargeRate({
+      ...enlargeRate,
+      x: e.target.value
+    });
+  };
+
+  const handleChangeEnlargeRateY = e => {
+    e.preventDefault();
+    setEnlargeRate({
+      ...enlargeRate,
+      y: e.target.value
+    });
+  };
+
   return (
     <Box>
       {/* 調整參數bar */}
@@ -29,16 +119,56 @@ const Magnifier = props => {
           </Heading>
         </Box>
         <Flex px={3} flex={1}>
-          <Slider title="縮圖比例" min={0.1} max={1} step={0.1} value={0.1} />
-          <Slider title="檢視寬度" min={1} max={3} step={0.5} value={1} />
-          <Slider title="檢視高度" min={1} max={2} step={0.5} value={1} />
+          <Slider
+            title="縮圖比例"
+            min={0.05}
+            max={0.12}
+            step={0.01}
+            value={smallImageRate}
+            onChange={e => handleSmallImageRate(e)}
+          />
+          <Slider
+            title="檢視寬度"
+            min={1}
+            max={5}
+            step={0.1}
+            value={enlargeRate.x}
+            onChange={e => handleChangeEnlargeRateX(e)}
+          />
+          <Slider
+            title="檢視高度"
+            min={1}
+            max={2}
+            step={0.1}
+            value={enlargeRate.y}
+            onChange={e => handleChangeEnlargeRateY(e)}
+          />
+          <Box mt={4}>
+            <StyledLink
+              to="/"
+              color="text"
+              border="1px solid"
+              fontSize={4}
+              px={2}
+              py={1}
+            >
+              Back
+            </StyledLink>
+          </Box>
         </Flex>
       </Flex>
 
       {/* 小圖大圖 */}
-      <Box p={3}>
-        <Heading>Image</Heading>
-      </Box>
+      <Flex p={3} justifyContent="center">
+        <Box width={['100vw', '100vw', '100vw']}>
+          <ImageView
+            smallImageRate={smallImageRate}
+            oriImageDimention={oriImageDimention}
+            enlargeRate={numToPercent(enlargeRate)}
+          />
+          {/* <Pre>{JSON.stringify(oriImageDimention, null, 2)}</Pre> */}
+        </Box>
+      </Flex>
     </Box>
   );
 };
--- src/components/style.js
+++ src/components/style.js
@@ -1,7 +1,10 @@
+import React from 'react';
+
 import { createGlobalStyle } from 'styled-components';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { color, fontSize, space } from 'styled-system';
+import { color, fontSize, space, border } from 'styled-system';
+import { Text } from 'rebass';
 
 export const GlobalStyle = createGlobalStyle`
   *,*::before, *::after {
@@ -20,12 +23,19 @@ export const GlobalStyle = createGlobalStyle`
 `;
 
 export const StyledLink = styled(Link)`
+  width: 100%;
   text-decoration: none;
+  ${space}
   ${color}
+  ${border}
+  ${fontSize}
 `;
 
 StyledLink.defaultProps = {
-  color: 'primaryText'
+  color: 'primaryText',
+  border: 'none',
+  px: 0,
+  py: 0
 };
 
 export const Label = styled.label`
@@ -44,3 +54,14 @@ Label.defaultProps = {
 export const Input = styled.input`
   width: 100%;
 `;
+
+export const Pre = props => (
+  <Text
+    {...props}
+    as="pre"
+    fontFamily="Menlo, monospace"
+    p={2}
+    mt={2}
+    color="secondaryText"
+  />
+);
